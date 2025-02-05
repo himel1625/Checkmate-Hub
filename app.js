7fea60850c49da8acb1e5f0d5f45910f14167969
himel1625 <himelmia1625@gmail.com>
2025-02-05 23:56:43 +0600

    feat: Initial setup Chess.js,Ejs , socket.io

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const express = require('express');
+
+const socket = require('socket.io');
+const http = require('http');
+const { Chess } = require('chess.js');
+const path = require('path');
+const { title } = require('process');
 const app = express();
 const port = process.env.PORT || 4000;
 
+const server = http.createServer(app);
+const io = socket(server);
+
+const chess = new Chess();
+let player = {};
+let currentPlayer = 'W';
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('index', {
+    title: 'Checkmate-Hub',
+  });
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
